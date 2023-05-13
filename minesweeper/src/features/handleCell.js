873f2ed921dc4b9d6623144641a8c8fcef57b9d5
evgueniazet <zelenkoea19@gmail.com>
2023-05-13 12:06:24 +0200

    refactor: change icon files

--- minesweeper/src/features/handleCell.js
+++ minesweeper/src/features/handleCell.js
@@ -1,3 +1,6 @@
+import { iconLose } from "../icons/inocLose";
+import { iconFlag } from "../icons/iconFlag";
+
 const handleCell = () => {
 
     const cells = document.querySelectorAll('.cell');
@@ -8,16 +11,12 @@ const handleCell = () => {
 
         cell.addEventListener('click', () => {
             if (cell.hasAttribute('data-is-lose')) {
-                cell.classList.add('cell-lose');
+                cell.innerHTML = iconLose;
             }
         })
 
         cell.addEventListener('contextmenu', (e) => {
             e.preventDefault();
-            const iconFlag = '<svg class="icon" fill="#000000" width="800px" height="800px" viewBox="0 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg">\
-            <title>alt-flag</title>\
-            <path d="M0 30.016q0 0.832 0.576 1.408t1.44 0.576 1.408-0.576 0.576-1.408v-28q0-0.832-0.576-1.408t-1.408-0.608-1.44 0.608-0.576 1.408v28zM6.016 18.016h9.984v4h16l-4-8 4-8h-12v-4h-13.984v16z"></path>\
-            </svg>';
 
             if (!isFlag) {
                 cell.innerHTML = iconFlag;
