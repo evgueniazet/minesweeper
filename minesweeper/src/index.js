3cc6bdfd5a0ae8b8551e435c52146a8794407eca
evgueniazet <zelenkoea19@gmail.com>
2023-05-13 11:33:25 +0200

    feat: add cell select

--- minesweeper/src/index.js
+++ minesweeper/src/index.js
@@ -22,21 +22,39 @@ const addMinesweeper = () => {
     createInfo();
 
     const createMatrix = () => {
-        const matrix = [];
 
-        for (let i = 0; i < 10; i++) {
-            const row = [];
-            const randomIndex = Math.floor(Math.random() * 10);
 
-            for (let k = 0; k < 10; k++) {
-                if (k === randomIndex) {
-                    row.push(2);
-                } else {
-                    row.push(1);
+        const rows = 10;
+        const cols = 10;
+        const bombsCount = 10;
+
+        let matrix = new Array(rows);
+
+        for (let i = 0; i < rows; i++) {
+            matrix[i] = new Array(cols).fill(0);
+        }
+
+        let bombsPlaced = 0;
+        while (bombsPlaced < bombsCount) {
+            const row = Math.floor(Math.random() * rows);
+            const col = Math.floor(Math.random() * cols);
+
+            if (matrix[row][col] === -1) {
+                continue;
+            }
+
+            matrix[row][col] = -1;
+
+            for (let i = row - 1; i <= row + 1; i++) {
+                for (let j = col - 1; j <= col + 1; j++) {
+                    if (i >= 0 && i < rows && j >= 0 && j < cols && matrix[i][j] !== -1) {
+                        matrix[i][j]++;
+                    }
                 }
             }
-            matrix.push(row);
+            bombsPlaced++;
         }
+        console.log(matrix);
         return matrix;
     };
 
@@ -49,19 +67,18 @@ const addMinesweeper = () => {
                 const cell = document.createElement('div');
                 container.appendChild(cell);
                 cell.classList.add('cell');
-                if (matrix[i][j] === 2) {
-                    // cell.classList.add('cell-lose');
+                if (matrix[i][j] === -1) {
                     cell.dataset.isLose = true;
                 }
             }
         }
-
-
     };
 
     renderCell(matrix);
-
     handleCell();
 };
-
 addMinesweeper();
+
+
+
+
